b9d135f606ffbc016b009f5c97a720b75faed2cf
Wentana Ghile <137902774+wghile@users.noreply.github.com>
2023-12-15 01:31:02 -0500

    styled basic set up of phonebook

--- src/components/PhoneBook.jsx
+++ src/components/PhoneBook.jsx
@@ -1,7 +1,15 @@
 export default function PhoneBook() {
   return (
     <div className='PhoneBook'>
-        PhoneBook
+        <div id='book-container'>
+          <h1>
+            Contacts
+          </h1>
+          <input />
+          <div id='contacts'>
+            Info 
+          </div>
+        </div>
     </div>
   )
 }
